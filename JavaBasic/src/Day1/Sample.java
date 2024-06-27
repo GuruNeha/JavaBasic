69bcd28cfa8697426c763f1d1f099ac33c1c72be
Neha.k <Neha.k@DESKTOP-457PCN7.mshome.net>
2024-06-27 11:05:46 +0530

    This is my first project posting in github. This is my basic java project.

---
+++ JavaBasic/src/Day1/Sample.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("...");
+		System.out.println("Welcome to FSJ");
+	}
+
+}
---
+++ JavaBasic/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Neha.k
+ *
+ */
+module JavaBasic {
+}
